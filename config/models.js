c324065ff7b44fc700e5d7884729c83edef3b231
frontendgineer <frontendgineer@gmail.com>
2017-01-01 01:57:46 +0100

    add mysql db connection

--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  // connection: 'localDiskDb',
+  connection: 'nodeFinancialsMysqlServer',
 
   /***************************************************************************
   *                                                                          *
@@ -27,6 +27,6 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+  migrate: 'alter'  //allow sails to alter the structure of the database
 
 };
