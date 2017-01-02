6060a5931d0a765e66bfc315e75921ea0520d076
frontendgineer <frontendgineer@gmail.com>
2017-01-02 20:28:37 +0100

    add edit functionality

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -33,5 +33,23 @@ module.exports = {
                 customers: customers
             });
         });
+    },
+    edit: function (req, res, next) {
+        Customer.findOne(req.param('id'), function editedCustomer(err, customer) {
+            if (err) return next(err);
+            if (!customer) return next();
+
+            res.view({
+                customer: customer
+            });
+        });
+    },
+    update: function (req, res, err) {
+        Customer.update(req.param('id'), req.params.all(), function (err) {
+            if (err) {
+                return res.redirect('/customer/edit/' + req.param('id'));
+            }
+            res.redirect('/customer');
+        })
     }
 };
