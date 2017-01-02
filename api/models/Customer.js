f7aafe27e9ee7f5337497dbb2e75bc2fb1bca050
frontendgineer <frontendgineer@gmail.com>
2017-01-02 21:10:28 +0100

    ability to add stock and shares

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -18,6 +18,10 @@ module.exports = {
     },
     state: {
       type: 'string'
+    },
+    stocks: {  // one-to-many relationship: one customer can own many stocks
+      collection: 'stock', //tells us what model to use, in this case Stock
+      via: 'owner' // tells us which attribute of Stock is going to connect us back to Customer
     }
   }
 };
